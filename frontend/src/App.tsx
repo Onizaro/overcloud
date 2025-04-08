4a3fd88bc28a998c5c3a2c7c4243fbf1f36abd6d
Elias Nieres-tavernier <elias.nieres-tavernier@edu.devinci.fr>
2025-04-08 19:43:00 +0200

    features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,36 +5,38 @@ import Navbar from './components/Navbar/Navbar';
 import DocumentPage from './components/DocumentPage/DocumentPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import Features from './components/Features/Features'; // Importez le composant Features
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Navbar">
-        <Navbar />
+    <Router> {/* Le Router doit envelopper toute l'application */}
+      <div className="App">
+        <div className="Navbar">
+          <Navbar />
+        </div>
+        <div className="main-content">
+          <div className="Sidebar">
+            <Sidebar />
+          </div>
+          <div className="content-area">
+            <Routes>
+              <Route path="/" element={<DocumentPage />} /> {/* Page d'accueil */}
+              <Route path="/documents" element={<DocumentPage />} />
+              <Route path="/features" element={<Features />} /> {/* Nouvelle route pour Features */}
+              <Route
+                path="/login"
+                element={<div className="top-container"><LoginPage /></div>}
+              />
+              <Route
+                path="/register"
+                element={<div className="top-container"><RegisterPage /></div>}
+              />
+            </Routes>
+          </div>
+        </div>
       </div>
-      <div className="main-content">
-  <div className="Sidebar">
-    <Sidebar />
-  </div>
-  <div className="content-area">
-    <Router>
-      <Routes>
-        <Route path="/documents" element={<DocumentPage />} />
-        <Route
-          path="/login"
-          element={<div className="top-container"><LoginPage /></div>}
-        />
-        <Route
-          path="/register"
-          element={<div className="top-container"><RegisterPage /></div>}
-        />
-      </Routes>
     </Router>
-  </div>
-</div>
-
-    </div>
   );
 }
 
